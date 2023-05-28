a85d022ecc633dc4b5d95d331d790ced17d63afa
rashidulub <rashidul016094@gmail.com>
2023-05-28 18:55:59 +0600

    add toy data recive to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 // hello
@@ -33,12 +33,53 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+   
+
+    const serviceCOllection = client.db('toyMarket').collection('services');
+    const addtoy = client.db('toyMarket').collection('addtoy');
+
+    app.get('/addtoy',async(req,res)=>{
+      const cursor = addtoy.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+   
+    app.post('/addtoy', async(req,res)=>{
+      const newToys = req.body;
+      console.log(newToys);
+      const result = await addtoy.insertOne(newToys);
+      res.send(result)
+    })
+
+    app.get('/services',async(req,res)=>{
+      const cursor = serviceCOllection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+  
+    app.get('/services/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query ={ _id: new ObjectId(id)}
+
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {  title: 1, name: 1 , image: 1, category: 1,details: 1,price: 1},
+      };
+
+
+      const result = await serviceCOllection.findOne(query,options);
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
